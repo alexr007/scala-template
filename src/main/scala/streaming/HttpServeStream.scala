ac049e28976be8e985620e340fd875bc4fa32b1b
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2023-08-10 15:38:37 +0300

    -- update 2023.08.10

--- src/main/scala/streaming/HttpServeStream.scala
+++ src/main/scala/streaming/HttpServeStream.scala
@@ -4,10 +4,9 @@ import cats.effect.IO
 import cats.effect.IOApp
 import fs2.Stream
 import io.circe.syntax.EncoderOps
+import org.http4s.HttpRoutes
 import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.dsl.io._
-import org.http4s.HttpRoutes
-
 import scala.concurrent.duration.DurationInt
 import scala.util.Random
 
@@ -20,15 +19,23 @@ object HttpServeStream extends IOApp.Simple {
       .map(_ => Data(Random.nextInt(100)))
       .take(5)
 
-  // pack them as array
-  import org.http4s.circe.streamJsonArrayEncoder
+  val jsons = stream.map(_.asJson)
+
   val route: HttpRoutes[IO] = HttpRoutes.of[IO] {
     /** just test response */
-    case GET -> Root / "t" => Ok("test")
-    /** serve them as separate elements */
-    case GET -> Root / "s" => Ok(stream.map(_.asJson.noSpaces))
+    case GET -> Root / "t" =>
+      import org.http4s.circe.jsonEncoder
+      Ok("test".asJson)
+
     /** serve them as an array */
-    case GET -> Root / "a" => Ok(stream.map(_.asJson))
+    case GET -> Root / "a" =>
+      import org.http4s.circe.streamJsonArrayEncoder
+      Ok(jsons)
+
+    /** serve them as separate elements */
+    case GET -> Root / "s" =>
+      Ok(jsons.map(_.noSpaces))
+
   }
 
   override def run = BlazeServerBuilder[IO]
